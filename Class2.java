d553c41a9884c2dcee1122f493ec7548ae1b1bbf
謙謙 <62059488+QianQianHuang@users.noreply.github.com>
2021-03-24 11:40:18 +0800

    Create Class2.java

---
+++ Class2.java
@@ -0,0 +1,19 @@
+public class Class2 {
+
+    public static void main(String[] args) {
+        String input = "Hello my name is Qian";
+
+        int key = 3;
+
+        char[] chars = input.toCharArray();
+        StringBuilder ZN = new StringBuilder();
+        for (char aChar : chars) {
+            int b = aChar;
+
+            b = b + 3;
+            char newb = (char) b;
+            ZN.append(newb);
+        }
+        System.out.println("密文===" + ZN.toString());
+    }
+}
